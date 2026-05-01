c1b15aed6a89b22aac5c5d4abc442d410adc6db4
Ayau <khaldybekaiaulym@gmail.com>
2026-05-01 21:59:19 +0000

    feat: complete infrastructure with FastAPI, React, and Nginx proxy

--- backend/main.py
+++ backend/main.py
@@ -1,23 +1,32 @@
 from fastapi import FastAPI
-import os
 import psycopg2
+from psycopg2.extras import RealDictCursor
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-@app.get("/")
-def home():
-    return {"message": "Бэкенд работает!"}
+# Разрешаем фронтенду обращаться к бэкенду
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
-@app.get("/db-test")
-def test():
-    try:
-        # Эти данные мы пропишем позже в Docker
-        conn = psycopg2.connect(
-            host=os.getenv('DB_HOST', 'db'),
-            database=os.getenv('DB_NAME', 'lab_db'),
-            user=os.getenv('POSTGRES_USER', 'user'),
-            password=os.getenv('POSTGRES_PASSWORD', 'password')
-        )
-        return {"status": "Связь с базой данных есть!"}
-    except:
-        return {"status": "База данных пока не видна"}
+def get_db_connection():
+    return psycopg2.connect(
+        host="db", # Имя сервиса в docker-compose
+        database="lab_db",
+        user="user",
+        password="password"
+    )
+
+@app.get("/parts")
+def get_parts():
+    conn = get_db_connection()
+    cur = conn.cursor(cursor_factory=RealDictCursor)
+    cur.execute('SELECT * FROM tuning_parts;')
+    parts = cur.fetchall()
+    cur.close()
+    conn.close()
+    return parts
